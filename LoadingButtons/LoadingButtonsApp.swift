61ee69a4315457d34a4a4db0ea3b5c2d188d9c94
Brad Ball <brad.ball@twinspires.com>
2023-09-07 20:20:54 -0400

    Initial Commit

---
+++ LoadingButtons/LoadingButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingButtonsApp.swift
+//  LoadingButtons
+//
+//  Created by Brad Ball on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LoadingButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
